2516f311a4807c992ce497a2c0dfd04ca9127da6
nqlllbot-cyber <nqlllbot@gmail.com>
2026-04-24 04:40:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,6 @@ FORCE_SUB_CHANNEL = os.environ.get('FORCE_SUB_CHANNEL', '')
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())
-
     conn = sqlite3.connect('bot_data.db')
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS users
